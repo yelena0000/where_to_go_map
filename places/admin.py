d3028b2776af1ffef26f09e1b87a47241479b835
yelena0000 <kirillnek07081999@gmail.com>
2025-03-13 17:28:57 +0300

    Setup media files serving

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, PlaceImage
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ('place', 'image', 'order')
+    ordering = ['place', 'order']
--- places/models.py
+++ places/models.py
@@ -9,3 +9,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name="Локация")
+    image = models.ImageField(upload_to='places_images/', verbose_name="Изображение")
+    order = models.PositiveIntegerField(default=0, verbose_name="Порядок")
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"{self.order} - {self.place.title}"
