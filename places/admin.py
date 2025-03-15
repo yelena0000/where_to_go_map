0842bc78071b31ce25815982d6b9eb90eaddd943
yelena0000 <kirillnek07081999@gmail.com>
2025-03-15 20:25:36 +0300

    Add PlaceImageInline

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
 
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+    fields = ['image', 'order',]
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
     list_display = ('title',)
 
 
