66584597bf62a70c87cda4858217e733dbddd108
yelena0000 <kirillnek07081999@gmail.com>
2025-03-15 21:39:04 +0300

    Make inline images sortable

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 from .models import Place, PlaceImage
 
 
-class PlaceImageInline(admin.TabularInline):
+class PlaceImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = PlaceImage
     extra = 1
     fields = ['image', 'image_preview', 'order',]
@@ -11,22 +12,12 @@ class PlaceImageInline(admin.TabularInline):
 
     def image_preview(self, obj):
         if obj.image:
-            return format_html(
-                '<img src="{}" style="max-height: 200px;" />',
-                obj.image.url
-            )
+            return format_html('<img src="{}" style="max-height: 200px;" />', obj.image.url)
         return "(No image)"
 
     image_preview.short_description = "Превью"
 
-
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):  # Наследуемся от SortableAdminBase
     inlines = [PlaceImageInline]
     list_display = ('title',)
-
-
-@admin.register(PlaceImage)
-class PlaceImageAdmin(admin.ModelAdmin):
-    list_display = ('place', 'image', 'order')
-    ordering = ['place', 'order']
