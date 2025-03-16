d8a3c6044ee3668382473cac802bc9197060102f
yelena0000 <kirillnek07081999@gmail.com>
2025-03-16 14:23:16 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
