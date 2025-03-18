39e43216525c07b79fea7567034804050f2060a0
yelena0000 <kirillnek07081999@gmail.com>
2025-03-18 03:40:54 +0300

    Update views.py

--- places/views.py
+++ places/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+
 from .models import Place
+
 
 def index(request):
     places = Place.objects.all()
@@ -19,7 +20,7 @@ def index(request):
                 "title": place.title,
                 "placeId": place.id,
                 "detailsUrl": reverse('place_detail', args=[place.id]),
-            }
+            },
         })
 
     geojson = {
@@ -35,7 +36,7 @@ def get_place_details(request, place_id):
     images = place.images.all()
     imgs = [image.image.url for image in images]
 
-    data = {
+    place_content = {
         'title': place.title,
         'imgs': imgs,
         'description_short': place.description_short,
@@ -43,4 +44,5 @@ def get_place_details(request, place_id):
         'latitude': place.latitude,
         'longitude': place.longitude,
     }
-    return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+
+    return JsonResponse(place_content, json_dumps_params={'ensure_ascii': False, 'indent': 4})
