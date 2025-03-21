bf3bde9c56ca47820961631b3ee2352458672abc
yelena0000 <kirillnek07081999@gmail.com>
2025-03-22 00:42:41 +0300

    Fix some bugs

--- places/models.py
+++ places/models.py
@@ -1,28 +1,31 @@
-from tinymce.models import HTMLField
 from django.db import models
+from tinymce.models import HTMLField
+
 
 class Place(models.Model):
     title = models.CharField(
         max_length=200,
-        verbose_name="Название"
+        verbose_name='Название',
+        unique=True
     )
-    description_short = models.TextField(
-        verbose_name="Краткое описание"
+    short_description = models.TextField(
+        verbose_name='Краткое описание',
+        blank=True
     )
-    description_long = HTMLField(
-        verbose_name="Полное описание",
+    long_description = HTMLField(
+        verbose_name='Полное описание',
         blank=True
     )
     latitude = models.FloatField(
-        verbose_name="Широта"
+        verbose_name='Широта'
     )
     longitude = models.FloatField(
-        verbose_name="Долгота"
+        verbose_name='Долгота'
     )
 
     class Meta:
-        verbose_name = "Локация"
-        verbose_name_plural = "Локации"
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
 
     def __str__(self):
         return self.title
@@ -33,21 +36,24 @@ class PlaceImage(models.Model):
         Place,
         on_delete=models.CASCADE,
         related_name='images',
-        verbose_name="Локация"
+        verbose_name='Локация'
     )
     image = models.ImageField(
         upload_to='places_images/',
-        verbose_name="Изображение"
+        verbose_name='Изображение',
+        blank=True,
+        null=True
     )
     order = models.PositiveIntegerField(
         default=0,
-        verbose_name="Порядок"
+        db_index=True,
+        verbose_name='Порядок'
     )
 
     class Meta:
-        verbose_name = "Изображение"
-        verbose_name_plural = "Изображения"
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
         ordering = ['order']
 
     def __str__(self):
-        return f"{self.order} - {self.place.title}"
+        return f'{self.order} - {self.place.title}'
--- places/views.py
+++ places/views.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.db.models import Prefetch
 
-from .models import Place
+from .models import Place, PlaceImage
 
 
 def index(request):
@@ -11,36 +12,41 @@ def index(request):
     features = []
     for place in places:
         features.append({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.longitude, place.latitude],
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude],
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": reverse('place_detail', args=[place.id]),
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': reverse('place_detail', args=[place.id]),
             },
         })
 
     geojson = {
-        "type": "FeatureCollection",
-        "features": features,
+        'type': 'FeatureCollection',
+        'features': features,
     }
 
     return render(request, 'index.html', {'geojson': geojson})
 
 
 def get_place_details(request, place_id):
-    place = get_object_or_404(Place, id=place_id)
-    images = place.images.all()
-    imgs = [image.image.url for image in images]
+    place = get_object_or_404(
+        Place.objects.prefetch_related(
+            Prefetch('images', queryset=PlaceImage.objects.all())
+        ),
+        id=place_id
+    )
+
+    imgs = [image.image.url for image in place.images.all()]
 
     place_content = {
         'title': place.title,
         'imgs': imgs,
-        'description_short': place.description_short,
-        'description_long': place.description_long,
+        'description_short': place.short_description,
+        'description_long': place.long_description,
         'latitude': place.latitude,
         'longitude': place.longitude,
     }
